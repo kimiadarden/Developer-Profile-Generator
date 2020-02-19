648f9556d09fec58f6f2a8e53e1c41c604838ed4
Kimia Darden <kimiadarden@gmail.com>
2020-02-19 09:29:01 -0500

    changing the method used for displaying the avatar

--- Readmee.md
+++ Readmee.md
@@ -1,54 +1,41 @@
 
-
-  #   Title :kimia
-
-
-   ## Description
-
-   
-
-
-  ## Installation
-
   
+  #   Title :hgfds
+  
+  ----
 
+  ## Description
+  
+  jhgfds
+  
+  ----
+  ## Installation
+  
+  jhgfds
+  
+  ----
 
-   ## Usage
-
-   
-
+  ## Usage
+  
+  
+  > hgfd
+  
+  ----
 
   ## Contributing
+  ----
 
   ## License
+  undefined
   
-  
-  
+  ----
+
   ## Author 
   
-  Github User name :  kimiadarden
-
-  Email  Adress:   
-
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Document</title>
-  </head>
-  <body>
-
-
-  <a href=" https://github.com/kimiadarden.png?size=200">
-  <img src=" https://github.com/kimiadarden.png?size=200" alt="avatar" height="48" width="48">
-   </a>
-         
-
+  Github User name :  gf
   
-  </body>
-  </html>
+  Email  Adress:   hgfd
 
-  
-  
-  
+ ![Github Avatar](https://github.com/gf.png?=20x20)
+
+   
--- index.js
+++ index.js
@@ -60,53 +60,41 @@ function generateReadme(answers) {
   
   #   Title :${answers.Title}
   
-  
+
   ## Description
   
   ${answers.Description}
   
-  
+  ----
   ## Installation
   
   ${answers.Installation}
   
-  
+  ----
+
   ## Usage
   
-  ${answers.Usage}
   
+  > ${answers.Usage}
   
+  ----
+
   ## Contributing
-  
+  ----
+
   ## License
-  ${answers.License}
+  ${answers.License.choices}
   
-  
+  ----
+
   ## Author 
   
   Github User name :  ${answers.username}
   
   Email  Adress:   ${answers.Email}
-  
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  </head>
-  <body>
 
-  
-  <a href=" https://github.com/${answers.username}.png?size=200">
-  <img src=" https://github.com/${answers.username}.png?size=200" alt="avatar" height="48" width="48">
-   </a>
-   
-   </body>
-   </html>
-   
-   
-   
+ ![Github Avatar](https://github.com/${answers.username}.png?=20x20)
+
    `;
   }
   
